4cc78f3d8e82af27266c376d1f7ae60ba3539b8f
Daniel O'Brien <20101948@setu.ie>
2025-09-17 12:35:13 +0100

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,4 +1,4 @@
-import {Friend, Colleague } from './myTypes'
+import {Friend, Colleague, EmailContact } from './myTypes'
 import { friends, colleagues } from './01-basics'
 
 function older(f: Friend) : string {
@@ -17,7 +17,7 @@ function allOlder(f : Friend[]) : string[] {
 // console.log(allOlder(friends))
 
 // Find the colleague with the highest extension number.
-function highestExtension(cs: Colleague[]): Colleague {
+function highestExtension(cs: Colleague[]) {
   const result = cs.sort(
     (c1, c2) => c1.contact.extension - c2.contact.extension
   );
@@ -33,5 +33,29 @@ function addColleague(cs : Colleague[], n : string, d : string, e : string): Col
   return cs;
 }
 
-addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
-console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+// addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+// console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+function sortColleagues(
+  colleagues: Colleague[],
+  sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+  const sorted = colleagues.sort(sorter); // Colleague[] inferred
+  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return result 
+}
+
+// console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+// console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+function findFriends(
+  friends: Friend[],
+  filter: (friend: Friend) => boolean
+): string[] {
+  const filtered = friends.filter(filter);
+  const result: string[] = filtered.map((f) => f.name );
+  return result
+}
+
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
